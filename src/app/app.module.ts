2f5b57edc9cdda3fbaf2400a20878f268d8d0330
sethias <sethias@HCL.COM>
2018-08-03 10:05:59 +0530

    Remove welcome module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { AppComponent } from './app.component';
-import { WelcomeModule } from './home/welcome.module';
 import { ProductModule } from './product/product.module';
 import { WelcomeComponent } from './home/welcome.component';
 import { HttpClientModule } from '@angular/common/http';
